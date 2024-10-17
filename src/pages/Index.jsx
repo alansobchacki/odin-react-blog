550b4923437af572a70f0569bac3c83bdacc1c5e
Alan Sobchacki <a.sobchack@gmail.com>
2024-10-16 22:31:11 -0300

    allow for admins to create and publish posts.

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -40,7 +40,7 @@ const MainPage = () => {
         posts.map((post) => (
           <div key={post.id}>
             <h2>{post.title}</h2>
-            <p>{post.body}</p>
+            <p>{post.content}</p>
           </div>
         ))
       ) : (
--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,15 +1,66 @@
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 import useAuth from '../hooks/Auth.jsx';
 
 const Posts = () => {
-  const { loggedIn, isAdmin, hasToken, userName } = useAuth();
   const navigate = useNavigate();
+  const apiUrl = import.meta.env.VITE_API_URL;
+  const { loggedIn, isAdmin, hasToken, userName } = useAuth();
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [published, setPublished] = useState(true); // temporary, tweak this logic later
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  
+    const token = localStorage.getItem("token");
+  
+    try {
+      const response = await fetch(`${apiUrl}/posts`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": `Bearer ${token}`,
+        },
+        body: JSON.stringify({ title, content, published }),
+      });
+  
+      const data = await response.json();
+  
+      if (response.ok) {
+        alert("Post created successfully!");
+      } else if (response.status === 401) { 
+        alert("You are not authorized to create a post.");
+      } else {
+        alert(`Failed to create post: ${data.message}`);
+      }
+    } catch (error) {
+      alert("Error: " + error.message);
+    }
+  };
 
   return (
     <>
       {loggedIn && hasToken && isAdmin && (
         <>
-          <p>Hello, {userName}. You can post stuff now!</p>
+          <p>Hello, {userName}.</p>
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              placeholder="Title"
+              required
+            />
+            <input
+              type="content"
+              value={content}
+              onChange={(e) => setContent(e.target.value)}
+              placeholder="Content"
+              required
+            />
+            <button type="submit">Create Post</button>
+          </form>
         </>
       )}
       {!loggedIn &&(
