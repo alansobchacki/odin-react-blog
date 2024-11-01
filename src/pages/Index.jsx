aca6260b7d61c9cb935e0fb3d4260421a5c6ed22
Alan Sobchacki <a.sobchack@gmail.com>
2024-11-01 11:58:44 -0300

    add support for commenting posts.

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -14,8 +14,12 @@ const apiUrl = import.meta.env.VITE_API_URL;
 
 const MainPage = () => {
   const [posts, setPosts] = useState([]);
+  const [content, setContent] = useState("");
+  const [commentAuthorId, setCommentAuthorId] = useState(0);
+  const currentUser = localStorage.getItem("user");
+  const token = localStorage.getItem("token");
 
-  const getPosts = async () => {
+  const getAllPosts = async () => {
     try {
       const response = await fetch(`${apiUrl}/`, {
         method: "GET",
@@ -26,6 +30,7 @@ const MainPage = () => {
 
       if (response.ok) {
         const data = await response.json();
+        console.log(data);
         setPosts(data);
       } else {
         console.error("Failed to fetch posts");
@@ -35,9 +40,43 @@ const MainPage = () => {
     }
   };
 
+  const writeNewComment = async (postId, author_id) => {
+    try {
+      console.log(`Trying to write a comment on post ${postId} from author ${author_id}`)
+
+      const response = await fetch(`${apiUrl}/comments/${postId}`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": `Bearer ${token}`,
+        },
+        body: JSON.stringify({ author_id, content }),
+      });
+
+      const data = await response.json();
+
+      if (response.ok) {
+        alert("Comment created successfully!");
+        setContent("");
+      } else if (response.status === 401) { 
+        alert("You are not authorized to write comments.");
+      } else {
+        alert(`Failed to create comment: ${data.message}`);
+      }
+    } catch (error) {
+      alert("Error: " + error.message);
+    }
+  };
+
   useEffect(() => {
-    getPosts();
-  }, []);
+    getAllPosts();
+
+    if (currentUser) {
+      const userData = JSON.parse(currentUser);
+      const authorId = userData.id;
+      setCommentAuthorId(authorId);
+    }
+  }, [currentUser]);
 
   return (
     <MainContainer>
@@ -48,6 +87,36 @@ const MainPage = () => {
             <BlogPostContainer key={post.id}>
               <BlogPostTitle>{post.title}</BlogPostTitle>
               <BlogPostMessage>{post.content}</BlogPostMessage>
+              <button>Comment</button>
+                <form
+                  onSubmit={(e) => {
+                    e.preventDefault();
+                    writeNewComment(post.id, commentAuthorId);
+                    console.log(post);
+                  }}
+                >
+                  <title>Login</title>
+                  <input
+                    type="text"
+                    value={content}
+                    onChange={(e) => setContent(e.target.value)}
+                    placeholder="Write your comment here."
+                    required
+                  />
+                  <button type="submit">Submit Comment</button>
+                </form>
+              <>
+                {post.comments && post.comments.length > 0 && (
+                  <div>
+                    <h4>Comments:</h4>
+                    {post.comments.map((comment) => (
+                      <div key={comment.id}>
+                        <p>{comment.content}</p>
+                      </div>
+                    ))}
+                  </div>
+                )}
+              </>
             </BlogPostContainer>
           ))
         ) : (
